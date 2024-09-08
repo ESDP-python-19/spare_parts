dac5f0f9fcc71803556c4688eab606c876188e8a
Kairat <ischenbaew@gmai.com>
2024-09-08 23:28:54 +0200

    реализовал view которое будет обрабатывать запросы и фильтровать данные(можно искать по запчастям Part или транспортным средствам VehicleInfo)

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .forms import SearchForm
+from .models.car import CarBrand
+from .models.part import Part
+from .models.vehicleinfo import VehicleInfo
 
-# Create your views here.
+
+def search_view(request):
+    form = SearchForm(request.GET or None)  # здесь получаем поисковый запрос
+    query = request.GET.get('query', '')  # здесь извлекаем строку запроса
+
+    part_results = Part.objects.none()  # пустой queryset для запчастей
+    vehicle_results = VehicleInfo.objects.none()  # пустой queryset для транспортных средств
+    car_results = CarBrand.objects.none() # для машин
+
+    if query:
+        part_results = Part.objects.filter(name__icontains=query)  # фильтрация по названию запчасти
+        vehicle_results = VehicleInfo.objects.filter(
+            model__name__icontains=query # фильтрация по модели транспортного средства
+        )
+        car_results = CarBrand.objects.filter(name__icontains=query)
+
+
+    context = {
+        'form': form,
+        'part_results': part_results,
+        'car_brands': car_results,
+        'vehicle_results': vehicle_results,
+    }
+    return render(request, 'search_results.html', context)
