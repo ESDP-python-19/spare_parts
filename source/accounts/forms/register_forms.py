c21e1b11e93eace328fdd7f36b65dfbb6ee182d8
Kairat <ischenbaew@gmai.com>
2024-09-18 23:34:47 +0200

    Создал форму регистрации

---
+++ source/accounts/forms/register_forms.py
@@ -0,0 +1,17 @@
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from accounts.models import User
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email уже используется')
+        return email
