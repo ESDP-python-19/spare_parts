8644c83b4123ddf1813f4c57dd2705c583d5d415
Kairat <ischenbaew@gmai.com>
2024-09-19 12:55:18 +0200

    Создал шаблоны регистрации и авторизации

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from accounts.views.views import register, UserLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', register, name='register'),
 ]
+
--- source/accounts/views/views.py
+++ source/accounts/views/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from accounts.forms.register_forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Регистрация прошла успешно!')
+            return redirect('webapp:parts_list')
+        else:
+            messages.error(request, 'Ошибка регистрации. Проверьте введенные данные.')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
