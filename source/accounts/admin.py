74b5a23903e24d20e713644e89b6c3199159ad10
Kairat <ischenbaew@gmai.com>
2024-09-19 13:00:38 +0200

    admin

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from accounts.models.user import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
