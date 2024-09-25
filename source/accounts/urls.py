19581ff95dd9146c8a04631d3982d1d40d5da68c
Kairat <ischenbaew@gmai.com>
2024-09-25 14:08:30 +0200

    Исправил пути

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
+
+from accounts.views.login import UserLoginView
+from accounts.views.logout import custom_logout_view
 from accounts.views.register import register_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', custom_logout_view, name='logout'),
 ]
